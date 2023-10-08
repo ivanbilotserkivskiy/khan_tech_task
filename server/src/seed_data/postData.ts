87025012daaa05ee81ef9902ee77368d2c8e6820
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-10-08 16:48:53 +0300

    fix post data

--- server/src/seed_data/postData.ts
+++ server/src/seed_data/postData.ts
@@ -4,7 +4,7 @@ export const postData = [
     description: "If you have tried everything, but still seem to suffer from snoring, don’t give up. Before turning to surgery, consider shopping for anti-snore devices. These products do not typically require a prescription, are economically priced and may just be the answer that you are looking for. However, as is the case when shopping for anything, there are a lot of anti-snore devices out there and…",
     realm: "Health",
     userId: 1,
-    image: "../public/images/beautiful-girl-is-engaged-yoga-studio.jpg",
+    image: "/images/beautiful-girl-is-engaged-yoga-studio.jpg",
     readTime: 6
   },
   {
@@ -12,7 +12,7 @@ export const postData = [
     description: "Swedish massage is a relaxing and therapeutic form of massage that focuses on improving circulation, easing tension, and promoting overall relaxation. When it comes to providing a soothing back massage, mastering the basic Swedish techniques is essential. Here are some fundamental techniques to get you started...",
     realm: "Health",
     userId: 1,
-    image: "../public/images/anti-cellulite-massage-luxury-spa.jpg",
+    image: "/images/anti-cellulite-massage-luxury-spa.jpg",
     readTime: 6
   },
   {
@@ -20,7 +20,7 @@ export const postData = [
     description: "Learning coding is an exciting journey that opens up a world of possibilities in the digital age. Whether you aspire to become a software developer, web designer, or simply want to understand the language of computers, here are some steps to get started on your coding adventure...",
     realm: "Digital",
     userId: 1,
-    image: "../public/images/close-up-man-writing-code-laptop.jpg",
+    image: "/images/close-up-man-writing-code-laptop.jpg",
     readTime: 6
   },
   {
@@ -28,7 +28,7 @@ export const postData = [
     description: "In recent years, concerns have arisen regarding the extent of Google's influence over think tanks and policy institutions. As one of the world's largest and most powerful technology companies, Google's financial contributions to these organizations have sparked debates about their independence and the potential impact on public policy. Here are some key points to consider...",
     realm: "Digital",
     userId: 1,
-    image: "../public/images/side-view-cropped-unrecognizable-business-people-working-common-desk.jpg",
+    image: "/images/side-view-cropped-unrecognizable-business-people-working-common-desk.jpg",
     readTime: 6
   },
   {
@@ -36,7 +36,7 @@ export const postData = [
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dictumst ut senectus ut purus odio amet lorem. Malesuada ridiculus elementum tempus, nullam. Morbi volutpat nulla vel proin viverra adipiscing. Mi euismod morbi congue malesuada. Et sed aliquet ullamcorper nibh amet vitae accumsan. Viverra arcu vitae risus pellentesque nec arcu. Feugiat habitant sodales vel.",
     realm: "Health",
     userId: 1,
-    image: "../public/images/yoga-group-classes-inside-gym.jpg",
+    image: "/images/yoga-group-classes-inside-gym.jpg",
     readTime: 6
   },
   {
@@ -44,7 +44,7 @@ export const postData = [
     description: "In the ever-evolving landscape of digital art and collectibles, Non-Fungible Tokens (NFTs) have emerged as a game-changer. NFTs are revolutionizing how we buy, sell, and own digital creations, from artwork to music to virtual real estate. But how do NFTs actually work, and what's behind their meteoric rise in popularity?",
     realm: "Digital",
     userId: 1,
-    image: "../public/images/half-holographic-skull-with-inscription-nft-nonfungible-token-digital-art.jpg",
+    image: "/images/half-holographic-skull-with-inscription-nft-nonfungible-token-digital-art.jpg",
     readTime: 6
   },
   {
@@ -52,7 +52,7 @@ export const postData = [
     description: "The fight against COVID-19 has seen several milestones, from the development of vaccines to their widespread distribution. Now, as we navigate the evolving landscape of the pandemic, a new question arises: should you consider getting vaccinated again? Here's what you need to know.",
     realm: "Medical",
     userId: 1,
-    image: "../public/images/side-view-doctor-holding-injection.jpg",
+    image: "/images/side-view-doctor-holding-injection.jpg",
     readTime: 6
   },
   {
@@ -60,7 +60,7 @@ export const postData = [
     description: "Traveling as a couple is a wonderful way to create lasting memories and strengthen your bond. Whether you're seeking romance, adventure, or relaxation, there are incredible destinations around the world that cater to couples. Here are 25 of the best worldwide vacation spots to consider for your next romantic getaway:",
     realm: "Travel",
     userId: 1,
-    image: "../public/images/loving-couple-walks-background-high-mountains-with-glaciers-peak.jpg",
+    image: "/images/loving-couple-walks-background-high-mountains-with-glaciers-peak.jpg",
     readTime: 6
   },
   {
@@ -68,7 +68,7 @@ export const postData = [
     description: "If you have tried everything, but still seem to suffer from snoring, don’t give up. Before turning to surgery, consider shopping for anti-snore devices. These products do not typically require a prescription, are economically priced and may just be the answer that you are looking for. However, as is the case when shopping for anything, there are a lot of anti-snore devices out there and…",
     realm: "Health",
     userId: 1,
-    image: "../public/images/two-girls-enjoing-yoga.jpg",
+    image: "/images/two-girls-enjoing-yoga.jpg",
     readTime: 6
   },
   {
@@ -76,7 +76,7 @@ export const postData = [
     description: "Aging is a natural process that has fascinated scientists for centuries. While the quest for the Fountain of Youth remains elusive, researchers are making significant strides in understanding the biology of aging and its potential to be influenced. Laboratory experiments are shedding light on the mechanisms of aging and offering hope for extending human lifespans.",
     realm: "Health",
     userId: 1,
-    image: "../public/images/man-in-the-laboratory.jpg",
+    image: "/images/man-in-the-laboratory.jpg",
     readTime: 6
   }
 ]
